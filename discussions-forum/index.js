708aab8428b34fcee93c21f6fcce8ec8d715539d
Balasubramanian U <ubalasubramanian03@gmail.com>
2022-03-05 21:42:42 +0530

    discussions-forum - deployment preparations

--- discussions-forum/index.js
+++ discussions-forum/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const passport = require("passport");
 const app = express();
+const path = require("path");
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 require("./startup/routes")(app);
 require("./startup/db")();
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || "5000";
 var server = app.listen(port, () => {
--- discussions-forum/startup/db.js
+++ discussions-forum/startup/db.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
 module.exports = function () {
+  // mongoose
+  //   .connect("mongodb://127.0.0.1:27017/discussions-forum", {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   })
+  //   .then(() => {
+  //     console.log("connected to mongodb");
+  //   })
+  //   .catch((err) => {
+  //     console.log("can't connect to mongodb", err);
+  //   });
   mongoose
-    .connect("mongodb://127.0.0.1:27017/discussions-forum", {
+    .connect(process.env.MONOGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
