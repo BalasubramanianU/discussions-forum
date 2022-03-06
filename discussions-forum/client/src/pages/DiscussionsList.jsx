6970b3243ce31b6c07fa3846fdef3f5d62a34760
Balasubramanian U <ubalasubramanian03@gmail.com>
2022-03-06 08:15:27 +0530

    deployment test

--- discussions-forum/client/src/pages/DiscussionsList.jsx
+++ discussions-forum/client/src/pages/DiscussionsList.jsx
@@ -14,24 +14,29 @@ const DiscussionsList = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getDiscussionsList()
-      .then((data) => {
-        dispatch(initializeDiscussionsList(data));
-        // dispatch(
-        //   updateDiscussionsList({
-        //     topic: "hello",
-        //     description: "this is an example",
-        //   })
-        // );
-        // dispatch(
-        //   updateComment({
-        //     topic: "dummy",
-        //     comment: "this is second comment example",
-        //   })
-        // );
-        // dispatch(storeAuthInfo({ userName: "bala", password: "123" }));
-      })
+    signUpUser({ userName: "test2", password: "12" })
+      .then(() =>
+        dispatch(storeAuthInfo({ userName: "test2", password: "12" }))
+      )
       .catch((error) => console.log(error));
+    // getDiscussionsList()
+    //   .then((data) => {
+    //     dispatch(initializeDiscussionsList(data));
+    // dispatch(
+    //   updateDiscussionsList({
+    //     topic: "hello",
+    //     description: "this is an example",
+    //   })
+    // );
+    // dispatch(
+    //   updateComment({
+    //     topic: "dummy",
+    //     comment: "this is second comment example",
+    //   })
+    // );
+    // dispatch(storeAuthInfo({ userName: "bala", password: "123" }));
+    // })
+    // .catch((error) => console.log(error));
   }, []);
 
   return (
