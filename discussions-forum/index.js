36db88fcfa058256ac4c921ca54e33d4dba2e5df
Balasubramanian U <ubalasubramanian03@gmail.com>
2022-03-06 08:10:41 +0530

    deploymen - test

--- discussions-forum/index.js
+++ discussions-forum/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const path = require("path");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "client", "build")));
+
+console.log("first", process.env.PORT);
+console.log("second", process.env.MONGODB_URI);
+console.log("third", process.env.NODE_ENV);
 
 require("./startup/routes")(app);
 require("./startup/db")();
--- discussions-forum/startup/db.js
+++ discussions-forum/startup/db.js
@@ -1,22 +1,16 @@
 const mongoose = require("mongoose");
 
+const uri = process.env.MONGODB_URI;
 module.exports = function () {
-  // mongoose
-  //   .connect("mongodb://127.0.0.1:27017/discussions-forum", {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   })
-  //   .then(() => {
-  //     console.log("connected to mongodb");
-  //   })
-  //   .catch((err) => {
-  //     console.log("can't connect to mongodb", err);
-  //   });
   mongoose
-    .connect(process.env.MONOGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      uri,
+      // || "mongodb://127.0.0.1:27017/discussions-forum"
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => {
       console.log("connected to mongodb");
     })
